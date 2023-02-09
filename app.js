f811948aa018f0d4e314a188b3b66de2215685a7
Ajay Shankar <Ajayshankar400@gmail.com>
2023-02-09 18:12:15 +0530

    done the input output processing

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var translateButton= document.getElementById('btn-translate');
 var txtInput=document.getElementById("txt-input");
 
+var output=document.getElementById("output");
+
 
 function clickEvent(){
-    console.log("clicked");
-    console.log("input",txtInput.value)
+   
+    output.innerText="asd asdjas dad"+txtInput.value;
 };
 
 translateButton.addEventListener("click",clickEvent)
