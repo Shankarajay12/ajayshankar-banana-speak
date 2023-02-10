dce4b49f112192882e2a2b59ceba39b2001ae8f5
Ajay Shankar <Ajayshankar400@gmail.com>
2023-02-10 13:00:46 +0530

    done only style is left

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 var translateButton= document.getElementById('btn-translate');
 var txtInput=document.getElementById("txt-input");
-
 var output=document.getElementById("output");
+
+//var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL="https://api.funtranslations.com/translate/minion.json";
+
+function getTranslatedURL(text){
+    return  serverURL+"?"+"text="+text;
+}
+
+function errorOccured( error)
+{
+    console.log("Error Occured"+error);
+    alert("Someting wroung with the serve ! Please try after sometime")
+}
 
 
 function clickEvent(){
    
-    output.innerText="asd asdjas dad"+txtInput.value;
+    var inputText=txtInput.value;
+    
+    fetch(getTranslatedURL(inputText))
+    .then(response=>response.json())
+    .then(json=>{
+        var outputText=json.contents.translated;
+        output.innerHTML=outputText;
+    })
+    .catch(errorOccured);
 };
 
 translateButton.addEventListener("click",clickEvent)
